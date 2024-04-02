690b96d23b8b90964534174a3dac7a1c7297ef28
Bowen Liang <liangbowen@gf.com.cn>
2024-04-02 11:11:09 +0800

    HuggingfaceHubProvider.get_client

--- api/core/model_runtime/model_providers/huggingface_hub/huggingface_hub.py
+++ api/core/model_runtime/model_providers/huggingface_hub/huggingface_hub.py
@@ -1,4 +1,7 @@
 import logging
+from typing import Any
+
+from huggingface_hub import InferenceClient
 
 from core.model_runtime.model_providers.__base.model_provider import ModelProvider
 
@@ -9,3 +12,10 @@ class HuggingfaceHubProvider(ModelProvider):
 
     def validate_provider_credentials(self, credentials: dict) -> None:
         pass
+
+    @staticmethod
+    def get_client(credentials: dict = None, **kwargs: Any) -> Any:
+        client = InferenceClient(
+            token=credentials['huggingfacehub_api_token'],
+        )
+        return client
